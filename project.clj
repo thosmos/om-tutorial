bd0408195302fcfd046e0df1c82f9e7d940ff60b
Thomas Spellman <thos37@gmail.com>
2015-11-11 02:13:38 -0800

    routing example

--- project.clj
+++ project.clj
@@ -1,10 +1,78 @@
 (defproject om-tutorial "0.1.0-SNAPSHOT"
   :description "My first Om program!"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
-                 [org.omcljs/om "1.0.0-alpha11"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [cljsjs/react "0.14.0-1"]
+                 [cljsjs/react-dom "0.14.0-1" :exclusions [cljsjs/react]]
+                 [org.omcljs/om "1.0.0-alpha12" :exclusions [cljsjs/react]]
+                 [sablono "0.3.6" :exclusions [cljsjs/react]]
                  [datascript "0.13.2"]
-                 [sablono "0.3.4"]
-                 [rum-reforms "0.4.0" ]
-                 [figwheel-sidecar "0.4.0" :scope "provided"]]
-  :clean-targets ["target/"])
+                 [rum-reforms "0.4.0"]
+                 [figwheel-sidecar "0.4.0" :scope "provided"]
+                 ]
+  ;:clean-targets ["target/"]
+
+  :plugins [[lein-cljsbuild "1.1.0"]]
+
+  :clean-targets ^{:protect false} ["target/" "resources/public/js/"]
+
+  ;:source-paths ["src"]
+
+
+  ;:hooks [leiningen.cljsbuild]
+  ;:prep-tasks ["compile" ["cljsbuild" "once"]]
+
+  ;:profiles {:dev {:source-paths ["dev"]
+  ;                 :dependencies [;[org.clojure/clojurescript "1.7.48"]
+  ;                                ;[thheller/shadow-build "1.0.0-123"]
+  ;                                ]}}
+
+  :cljsbuild {
+              :builds [
+
+                       ;{:id           "srender"
+                       ; :jar true
+                       ; :source-paths ["src"]
+                       ; ;:resource-paths ["resources" "dev-resources"]
+                       ; :main 'om-tutorial.core
+                       ; :compiler     {:output-to     "resources/public/js/renderable.js"
+                       ;                :output-dir    "resources/public/js/out-server-side"
+                       ;                ;:preamble      ["dev-resources/react.min.js"]
+                       ;                :pretty-print  true
+                       ;                :warnings      true
+                       ;                :optimizations :advanced}}
+
+                       {:id           "modules"
+                        ;:jar true
+                        :source-paths ["src"]
+                        :compiler
+                                      {:optimizations :advanced
+                                       ;:pretty-print  true
+                                       ;:warnings      true
+                                       ;:source-map    false
+                                       :main       "om-tutorial.core"
+                                       :asset-path "js/out"
+                                       :output-dir    "resources/public/js/out"
+                                       :output-to     "resources/public/main.js"
+                                       ;:modules       {
+                                       ;                :cljs-base {
+                                       ;                            :output-to "resources/public/js/base.js"
+                                       ;                            }
+                                       ;                :core
+                                       ;                           {:output-to "resources/public/js/core.js"
+                                       ;                            :entries   #{"om-tutorial.core"}}
+                                       ;                :foo
+                                       ;                           {:output-to "resources/public/js/foo.js"
+                                       ;                            :entries   #{"om-tutorial.foo"}
+                                       ;                            ;:depends-on #{:core}
+                                       ;                            }
+                                       ;                :render
+                                       ;                           {:output-to  "resources/public/js/render.js"
+                                       ;                            :entries    #{"om-tutorial.render"}
+                                       ;                            :depends-on #{:core :foo}
+                                       ;                            }}
+                                       }}
+
+                       ]
+              }
+  )
--- script/figwheel.clj
+++ script/figwheel.clj
@@ -2,16 +2,16 @@
          '[figwheel-sidecar.repl-api :as ra])
 
 (ra/start-figwheel!
-  {:figwheel-options {}
-   :build-ids ["dev"]
+  {:figwheel-options {:server-port 3450}
+   :build-ids        ["dev"]
    :all-builds
-                     [{:id "dev"
-                       :figwheel true
+                     [{:id           "dev"
+                       :figwheel     true
                        :source-paths ["src"]
-                       :compiler {:main 'om-tutorial.core
-                                  :asset-path "js"
-                                  :output-to "resources/public/js/main.js"
-                                  :output-dir "resources/public/js"
-                                  :verbose true}}]})
+                       :compiler     {:main       "om-tutorial.core"
+                                      :asset-path "js/out-fig"
+                                      :output-to  "resources/public/main.js"
+                                      :output-dir "resources/public/js/out-fig"
+                                      :verbose    true}}]})
 
 (ra/cljs-repl)
