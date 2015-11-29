882fe02115fe68e8e4b07e18bbbc893e23c5ab91
Thomas Spellman <thos37@gmail.com>
2015-11-28 17:33:33 -0800

    baseline - something that works

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -2,256 +2,111 @@
   (:require
     [goog.dom :as gdom]
     [om.next :as om :refer-macros [defui]]
-    [om.dom :as dom]
-    [cljs.pprint :as pprint]
-    [thosmos.core :as ts]
     [datascript.core :as d]
-    ;[shodan.console :as l :include-macros true]
-    )
+    [om.dom :as dom])
   (:require-macros
-    [cljs-log.core :as l :refer [debug info warn severe]])
-  )
+    [cljs-log.core :as l :refer [debug info warn severe]]))
 
 (enable-console-print!)
 
-(def conn
-  (d/create-conn
-    {:db/id          {:db/unique :db.unique/identity}
-     :db/ident       {:db/unique :db.unique/identity}
-     :list/items     {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
-     :dashboard/list {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
-     :type           {:db/index true}
-     :content/type   {:db/index true}}))
+;; -----------------------------------------------------------------------------
+;; DB
+
+(def init-data (d/create-conn {:root  {:db/unique :db.unique/identity}
+                               :items {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
+                               :current-user {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
+                               :current-user/email {:db/unique :db.unique/identity}}))
+
+(d/transact! init-data
+  [{:db/id -1
+    :title "Foo"}
+   {:db/id -2
+    :title "Bar"}
+   {:db/id -3
+    :title "Baz"}
+   {:db/id -4
+    :email "bob.smith@gmail.com"}
+   {:root  "my app root"
+    :items [-1 -2 -3]
+    :current-user -4}])
+
+;; At this point, we could link the current-user entity to every item.
+;; But this is not a solution. We want to access the current-user entity
+;; by query-lookup and not via attribute we have to add to every item entity.
 
 
-(d/transact! conn
-  [{:db/id        -1
-    :type         :type/content
-    :content/type :blog/post
-    :author       "Laura Smith"
-    :title        "A Post!"
-    :content      "Lorem ipsum dolor sit amet, quem atomorum te quo"
-    :favorites    0}
-   {:db/id        -2
-    :type         :type/content
-    :content/type :asset/photo
-    :title        "A Photo!"
-    :image        "photo.jpg"
-    :caption      "Lorem ipsum"
-    :favorites    0}
-   {:db/id        -3
-    :type         :type/content
-    :content/type :blog/post
-    :author       "Jim Jacobs"
-    :title        "Another Post!"
-    :content      "Lorem ipsum dolor sit amet, quem atomorum te quo"
-    :favorites    0}
-   {:db/id        -4
-    :type         :type/content
-    :content/type :asset/graphic
-    :title        "Charts and Stufff!"
-    :image        "chart.jpg"
-    :favorites    0}
-   {:db/id        -5
-    :type         :type/content
-    :content/type :blog/post
-    :author       "May Fields"
-    :title        "Yet Another Post!"
-    :content      "Lorem ipsum dolor sit amet, quem atomorum te quo"
-    :favorites    0}
-   {:db/id      -6
-    :db/ident   :content/list
-    :type       :type/list
-    :list/title "A List of Various Media"
-    :list/items [-1 -2 -3 -4 -5]
-    :list/type  :type/content}
-   {:db/id           -7
-    :db/ident        :dashboard/root
-    :type            :type/app
-    :dashboard/title "The Root"
-    :dashboard/list  -6}])
-
-;(d/q '[:find [?e] :where [?e :content/type :blog/post]] (d/db conn))
-
-(debug "LOADING ...")
+;; -----------------------------------------------------------------------------
+;; read
 
 (defmulti read om/dispatch)
 
+
 (defmethod read :default
-  [{:keys [state query] :as env} k _]
-  (let [db (d/db state)]                                    ;; CACHING!!!
-    (l/debug "READ DEFAULT key" k)
-    (l/debug "READ DEFAULT query" query)
-    ;(debug (keys env))
-    ;(d/q '[:find [(pull ?e [*]) ...] :where [?e :type :type/content]] (d/db conn))
-    {:value nil}
-    ))
-
-;;; assumed to be called with a query or :type lookup key, otherwise it would return everything in the DB
-(defmethod read :db/id
-  [{:keys [state query] :as env} key params]
-  (l/debug "READ DB/ID query" query)
-  (l/debug "READ DB/ID params" params)
-  {:value nil}
-)
-
-;;; assumed to be called with a query or :type lookup key, otherwise it would return everything in the DB
-(defmethod read :db/ident
-  [{:keys [state query] :as env} key params]
-  (l/debug "DB/IDENT ast" (:ast env))
-  (l/debug "DB/IDENT query" query)
-  (l/debug "DB/IDENT params" params)
-  (let [value (d/pull (d/db state) query [:db/ident (:ident-ref params)])]
-    (debug "DB/IDENT result" value)
-    {:value value})
+  [{:keys [query state ast]} k _]
+  (let [p (js/console.log "read :default" k query)]
+    {:value :default-value})
   )
-;(if (contains? st k)
-;  {:value (get st k)}
-;  {:remote true})
 
-;(defmethod read :posts
-;  [{:keys [state query] :as env} k _]
-;  (let [db (d/db state)]                                    ;; CACHING!!!
-;    (debug "READ :posts query" query)
-;    (debug "env keys " (keys env))
-;
-;    (let [posts (d/q '[:find [(pull ?e ?query) ...] :in $ ?query :where [?e :content/type :blog/post]] (d/db conn) query)]
-;      (debug "query results:" posts)
-;      {:value posts})))
+(defmethod read :items
+  [{:keys [query state ast]} k _]
+  (let [_ (debug "read :items" query)]
+    {:value
+     ;; Now comes the question - how to do this in datascript?
+     ;; simple datascript pull ? nope! -> [:current-user _] is not valid pull syntax
+     ;; (get (d/pull st [{k query}] [:root "my app root"]) k) is not a solution
+     ;; Solutions:
+     ;; - rewrite db->tree for use with datascript
+     ;; - rewirte d/pull to use the om.next patterns
+     ;; - ?
+     [{:db/id "1" :title "default value"}]}))
+
+
+;; -----------------------------------------------------------------------------
+;; Components
+
+(defui Item
+  static om/Ident
+  (ident [_ {:keys [:db/id]}] id)
+  static om/IQuery
+  (query [_]
+    '[:db/id :title [:current-user _]]) ;; NEW STUFF
+  Object
+  (render [this]
+    (let [{:keys [title current-user]} (om/props this)]
+      (dom/li nil
+        (dom/div nil title)
+        (dom/div nil (:email current-user))))))
+
+(def item (om/factory Item))
+
+(defui ItemTwo
+  static om/IQuery
+  (query [_]
+    '[:db/id :title])
+  Object
+  (render [this]
+    (let [{:keys [:db/id title]} (om/props this)]
+      (dom/li nil
+        (dom/div nil (str id " - " title))))))
+
+(defui SomeList
+  static om/IQuery
+  (query [_]
+    [{:items (om/get-query ItemTwo)}])
+  Object
+  (render [this]
+    (dom/div
+      (dom/h2 nil "A List!")
+      (dom/ul nil
+        (map (om/factory ItemTwo) (-> this om/props :items))
+        ))))
+
+;; -----------------------------------------------------------------------------
+;; Setup
 
 (def reconciler
   (om/reconciler
-    {:state  conn
-     ;:normalize true
+    {:state init-data
      :parser (om/parser {:read read})}))
-;:send      (util/transit-post "/api")
 
-
-(defui Post
-  static om/Ident
-  (ident [this {:keys [:db/id] :as props}]
-    (let [_ (debug "Post Ident" id "params:" props)]
-      id))
-  static ts/ITypeKey
-  (type-key [this] {:content/type :blog/post})
-  static om/IQuery
-  (query [this]
-    ;'[*]
-    [:db/id :type :content/type :title :author :content])
-  Object
-  (render [this]
-    (let [query (om/get-query Post)
-          {:keys [title] :as props} (om/props this)]
-      (l/debug "Post props" props)
-      (l/debug "Post query" query)
-      (dom/div nil title
-        (dom/ul nil
-          (for [prop props]
-            (dom/li nil
-              (str (key prop) ": " (val prop)))))
-
-        ))))
-
- (defui Photo
-   static om/Ident
-   (ident [this {:keys [:db/id]}] id)
-   static ts/ITypeKey
-   (type-key [this] {:content/type :asset/photo})
-   static om/IQuery
-   (query [this]
-     [:db/id :type :title :image :caption])
-   Object
-   (render [this]
-     (dom/div nil "Photo")))
-
- (defui Graphic
-   static om/Ident
-   (ident [this {:keys [:db/id]}] id)
-   static ts/ITypeKey
-   (type-key [this] {:content/type :asset/graphic})
-   static om/IQuery
-   (query [this]
-     [:db/id :type :title :image])
-   Object
-   (render [this]
-     (dom/div nil "Graphic")))
-
- (defui ItemList
-   ;static om/Ident
-   ;(ident [this props] [:db/ident :content/list])
-   ;static ts/ITypeKey
-   ;(type-key [this] {:type :type/list})
-   ;static ts/ICollTypeKey
-   ;(coll-key [this] :content/type)
-   ;static om/IQueryParams
-   ;(params [this] {:id-ref nil})
-   static om/IQuery
-   (query [this]
-     ;[:db/id :list/title {:list/items (ts/get-union (with-meta [Post Photo Graphic] {:coll-key (ts/get-coll-key this)}) [:favorites])}]
-     `[
-       ;({:db/id [:db/id :list/title]} {:id-ref ?id-ref})
-      {:list/items
-       ;(ts/get-union )
-       {:post    ~(om/get-query Post)
-        :photo   ~(om/get-query Photo)
-        :graphic ~(om/get-query Graphic)}}])
-   Object
-   (render [this]
-     (dom/div nil
-       (get (om/props this) :list/title)
-       ;(ts/render this :list/items)
-       )))
-
- (defui Dashboard
-   static om/Ident
-   (ident [this props] [:db/ident :dashboard/root])
-   static om/IQuery
-   (query [this]
-     `[({:db/ident [:dashboard/title]} {:ident-ref :dashboard/root}) {:list/items ~(om/get-query ItemList)}])
- ;{:dashboard/list (ts/get-query ItemList)}
-   Object
-   (render [this]
-     (dom/div nil
-       (dom/div nil "TypeKey experiment")
-       (dom/br nil nil)
-       (dom/div nil (get-in (om/props this) [:db/ident :dashboard/title])))))
-       ;(ts/render this ItemList)
-
- (def dashboard (om/factory Dashboard))
-
-(defui App
-  static om/IQuery
-  (query [this]
-    [{:posts (om/get-query Post)}])
-  Object
-  (render [this]
-    (let [posts (:posts (om/props this))]
-      (dom/div nil
-        (dom/div nil "TypeKey experiment")
-        (dom/br nil nil)
-        ((om/factory ItemList))
-        (debug "ItemList QUERY" (om/get-query ItemList))
-        ;(for [post posts] ((om/factory Post) post))
-        ))))
-;(dashboard)
-; (js/console.log "component?"))))
-; (om/component?
-;   (om/factory Post))))))
-;(dashboard {:dashboard/title "yes"}))))))
-
-(def app (om/factory App))
-
-(om/add-root! reconciler Dashboard (gdom/getElement "app"))
-
-
-;(defui Campaign
-;  static om/Ident
-;  (ident [this {:keys [id]}] [:db/id id])
-;  static ts/ITypeKey
-;  (type-key [this] :dt/type)
-;  static om/IQuery
-;  (query [this]
-;    [:db/id :dt/type :c/name :c/desc :c/goal])
-;  Object
-;  (render [this]
-;    (dom/div nil "Campaign")))
+(om/add-root! reconciler SomeList (gdom/getElement "app"))
