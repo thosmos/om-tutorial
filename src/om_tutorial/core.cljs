2285cbe0664e5ca8a47dd50938313f8e1bf12a89
Thomas Spellman <thos37@gmail.com>
2015-12-05 15:41:41 -0800

    Change stuff

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -12,9 +12,9 @@
 ;; -----------------------------------------------------------------------------
 ;; DB
 
-(def init-data (d/create-conn {:root  {:db/unique :db.unique/identity}
-                               :items {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
-                               :current-user {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
+(def init-data (d/create-conn {:root               {:db/unique :db.unique/identity}
+                               :items              {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
+                               :current-user       {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
                                :current-user/email {:db/unique :db.unique/identity}}))
 
 (d/transact! init-data
@@ -26,8 +26,8 @@
     :title "Baz"}
    {:db/id -4
     :email "bob.smith@gmail.com"}
-   {:root  "my app root"
-    :items [-1 -2 -3]
+   {:root         "my app root"
+    :items        [-1 -2 -3]
     :current-user -4}])
 
 ;; At this point, we could link the current-user entity to every item.
@@ -38,8 +38,26 @@
 ;; -----------------------------------------------------------------------------
 ;; read
 
+(defn ds-parse [query]
+  (let [_ (debug "dslink query" query)]
+    (into []
+      (for [subq query]
+        (let [_ (debug "subq" subq)]
+          (if
+            (vector? subq)
+            (let [val `{~(first subq) [:db/id]}
+                  _ (debug "val" val)]))
+          )))
+    query))
+
 (defmulti read om/dispatch)
 
+(defmethod read :current-user
+  [{:keys [query state]} k p]
+  (let [_ (debug "read :current-user" query)
+        db @state
+        user (d/pull db '[*] )]
+    {:value user}))
 
 (defmethod read :default
   [{:keys [query state ast]} k _]
@@ -49,7 +67,9 @@
 
 (defmethod read :items
   [{:keys [query state ast]} k _]
-  (let [_ (debug "read :items" query)]
+  (let [_ (debug "read :items" query ast)
+        db @state
+        v (ds-parse query)]
     {:value
      ;; Now comes the question - how to do this in datascript?
      ;; simple datascript pull ? nope! -> [:current-user _] is not valid pull syntax
@@ -58,23 +78,34 @@
      ;; - rewrite db->tree for use with datascript
      ;; - rewirte d/pull to use the om.next patterns
      ;; - ?
-     [{:db/id "1" :title "default value"}]}))
+     [{:db/id "1" :title "defaultish value"}]}))
 
 
 ;; -----------------------------------------------------------------------------
 ;; Components
+
+(defui User
+  static om/Ident
+  (ident [_ {:keys [:db/id]}] id)
+  static om/IQuery
+  (query [_] [:db/id :email])
+  Object
+  (render [this]
+    (let [{:keys [db/id email]} (om/props this)]
+      ))
+  )
 
 (defui Item
   static om/Ident
   (ident [_ {:keys [:db/id]}] id)
   static om/IQuery
   (query [_]
-    '[:db/id :title [:current-user _]]) ;; NEW STUFF
+    '[:db/id :title [:current-user _]])                     ;; NEW STUFF
   Object
   (render [this]
-    (let [{:keys [title current-user]} (om/props this)]
+    (let [{:keys [db/id title current-user]} (om/props this)]
       (dom/li nil
-        (dom/div nil title)
+        (dom/div nil (str id " - " title))
         (dom/div nil (:email current-user))))))
 
 (def item (om/factory Item))
@@ -85,20 +116,20 @@
     '[:db/id :title])
   Object
   (render [this]
-    (let [{:keys [:db/id title]} (om/props this)]
+    (let [{:keys [db/id title]} (om/props this)]
       (dom/li nil
         (dom/div nil (str id " - " title))))))
 
 (defui SomeList
   static om/IQuery
   (query [_]
-    [{:items (om/get-query ItemTwo)}])
+    [{:items (om/get-query Item)}])
   Object
   (render [this]
     (dom/div
       (dom/h2 nil "A List!")
       (dom/ul nil
-        (map (om/factory ItemTwo) (-> this om/props :items))
+        (map (om/factory Item) (-> this om/props :items))
         ))))
 
 ;; -----------------------------------------------------------------------------
@@ -106,7 +137,7 @@
 
 (def reconciler
   (om/reconciler
-    {:state init-data
+    {:state  init-data
      :parser (om/parser {:read read})}))
 
 (om/add-root! reconciler SomeList (gdom/getElement "app"))
